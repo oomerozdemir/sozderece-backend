a3dc474b6bc4c44ca5b754de2dd7855d35398f62
Omer Ozdemir <oomerozdemir40@gmail.com>
2025-07-12 22:46:05 +0300

    [200~feat: ödeme sonrası e-posta bildirimi eklendi2

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import axios from "axios";
 import crypto from "crypto";
 import qs from "querystring";
-import { sendPaymentSuccessEmail } from "../../utils/sendEmail.js";
+import { sendPaymentSuccessEmail } from "../utils/sendEmail";
 
 
 const prisma = new PrismaClient();
