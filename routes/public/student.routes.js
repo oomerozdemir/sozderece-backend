46e6f7763dc77c12d420a153c07e7b9a9639d230
Omer Ozdemir <oomerozdemir40@gmail.com>
2025-09-08 21:04:33 +0300

    talep onay sonrasi mail gonderimi

--- routes/public/student.routes.js
+++ routes/public/student.routes.js
@@ -1,13 +1,27 @@
+// routes/public/student.routes.js
 import express from "express";
 import { authenticateToken } from "../../middleware/authMiddleware.js";
-import { getStudentProfile, completeAppointmentByStudent, getMyPastAppointmentsStudent, 
- } from "../../controllers/studentController.js";
+import {
+  getStudentProfile,
+  completeAppointmentByStudent,
+  getMyPastAppointmentsStudent,
+} from "../../controllers/studentController.js";
 
 const router = express.Router();
 
+/* Profil */
 router.get("/me", authenticateToken, getStudentProfile);
-router.patch("/student/v1/ogrenci/appointments/:id/complete", authenticateToken, completeAppointmentByStudent);
-router.get("/student/v1/ogrenci/me/appointments/past", authenticateToken, getMyPastAppointmentsStudent);
 
+router.patch(
+  "/v1/ogrenci/appointments/:id/complete",
+  authenticateToken,
+  completeAppointmentByStudent
+);
+
+router.get(
+  "/v1/ogrenci/me/appointments/past",
+  authenticateToken,
+  getMyPastAppointmentsStudent
+);
 
 export default router;
