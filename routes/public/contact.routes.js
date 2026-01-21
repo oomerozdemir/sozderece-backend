94ee503dbe76c1674018146238f1787842c5122f
Omer Ozdemir <oomerozdemir40@gmail.com>
2026-01-21 23:42:30 +0300

    contact new models

--- routes/public/contact.routes.js
+++ routes/public/contact.routes.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { createContact, createTrialMeeting } from "../../controllers/contact.controller.js";
+import { createContact } from "../../controllers/contact.controller.js";
 
 const router = express.Router();
 
 router.post("/", createContact); 
-router.post("/trial", createTrialMeeting);
 
 
 export default router;
