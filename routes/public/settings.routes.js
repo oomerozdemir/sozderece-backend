797df62bb663145908a6c02c3e5a88e031674697
Omer Ozdemir <oomerozdemir40@gmail.com>
2026-04-24 21:00:19 +0300

    admin panelinden kontrol

--- routes/public/settings.routes.js
+++ routes/public/settings.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getCountdown, getPopup, getPaymentPageSettings } from "../../controllers/siteSettings.controller.js";
+import { getCountdown, getPopup, getPaymentPageSettings, getEarlyRegistration } from "../../controllers/siteSettings.controller.js";
 
 const router = express.Router();
 
 router.get("/countdown", getCountdown);
 router.get("/popup", getPopup);
 router.get("/payment-page", getPaymentPageSettings);
+router.get("/early-registration", getEarlyRegistration);
 
 export default router;
